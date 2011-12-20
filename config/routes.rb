6ac55c159d1cb60720da9d5e54133aaba6b7bbbe
Steven Lee <steven.ky.lee@gmail.com>
2011-12-20 14:35:23 -0500

    static page test

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ SampleApp2::Application.routes.draw do
 
   get "pages/contact"
 
+  get "pages/about"
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
